216ec04b2d78e2191b98dc98282c8ab0af471cea
marine <marine@mistgreen.net>
2012-09-01 12:39:54 +0900

    XoopsX 17

--- anyware_xoops_pack201x_xampp177/xampp/xoops_trust_path/templates_c/localhost%2Fpack2011-default-basic5^%%1A^1A4^1A43A9BE%%db%3Alegacy_xoopsform_label.html.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2012-01-02 18:48:32
-         compiled from db:legacy_xoopsform_label.html */ ?>
-<?php echo $this->_tpl_vars['element']->getValue(); ?>
--- anyware_xoops_pack201x_xampp177/xampp/xoops_trust_path/templates_c/localhost%2Fpack2011-default-basic5^%%27^278^2784E41B%%db%3Asystem_dummy.html.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2012-01-03 14:28:45
-         compiled from db:system_dummy.html */ ?>
-<?php echo $this->_tpl_vars['dummy_content']; ?>
